90a103921fa81ffd68b8e1f9c60e155548e46f5a
Urizen <urizen@silver-rain.com>
2009-11-02 23:33:21 +0000

    End of day commit.

--- Classes/MoveMeController.m
+++ Classes/MoveMeController.m
@@ -11,4 +11,62 @@
 
 @implementation MoveMeController
 
+@synthesize list;
+
+- (IBAction) toggleMove {
+	
+	[self.tableView setEditing:!self.tableView.editing animated:YES];
+	
+	if (self.tableView.editing)
+		[self.navigationItem.rightBarButtonItem setTitle:@"Done"];
+	else
+		[self.navigationItem.rightBarButtonItem setTitle:@"Move"];
+	
+}
+
+-(void) viewDidLoad {
+	
+	
+	if (list == nil) {		
+		
+		NSArray *array = [[NSArray alloc] initWithObjects:@"Eeney", @"Meeny", @"Miney", @"Moe", @"Catch", @"A", @"Tiger", @"By",
+						  @"The", @"Toe", nil];
+		
+		self.list = array;
+		
+		[array release];
+	}
+	
+	UIBarButtonItem *moveButton = [[UIBarButtonItem alloc] initWithTitle:@"Move" style:UIBarButtonItemStyleBordered target:self action:@selector(toggleMove)];
+	
+	self.navigationItem.rightBarButtonItem = moveButton;
+	
+	[moveButton release];
+	[super viewDidLoad];
+}
+
+-(void) viewDidUnload {
+	
+	
+	self.list = nil;
+}
+
+- (void) dealloc {
+	
+	[list release];
+	[super dealloc];
+	
+}
+
+#pragma mark -
+#pragma mark Table Datasource Methods
+
+- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+	
+	
+	return [list count];
+}
+
+
+
 @end
